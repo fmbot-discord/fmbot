91b33c35c3a913cd51527987ea7b86a2f113108d
Bitl <toycrazy@hotmail.com>
2018-02-14 08:27:18 -0700

    fixed confilct

--- StartFMBot.bat
+++ StartFMBot.bat
@@ -1,8 +1,4 @@
 @ECHO OFF
 TITLE FMBot
 ECHO Loading...
-<<<<<<< HEAD:StartFMBot.bat
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\FMBotDaemon.ps1'"
-=======
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\FMBotDaemon.ps1'"
->>>>>>> origin/master:StartFMBot/StartFMBot.bat
