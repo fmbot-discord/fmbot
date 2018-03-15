7d85d70040d1e14606aacc108f0ff02e7108bab7
Bitl <toycrazy@hotmail.com>
2018-03-15 13:37:56 -0700

    Increase version number

--- FMBot_Discord/FMBot_Discord/Properties/AssemblyInfo.cs
+++ FMBot_Discord/FMBot_Discord/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FMBot_Discord")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Discord bot built with Discord.NET in C# used to access stats from last.fm.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bitl")]
 [assembly: AssemblyProduct("FMBot_Discord")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
