3bf50409c6952f1f1a63132643c5e7a5afbe203c
Michael Hart <bitlblox@gmail.com>
2018-03-29 11:59:15 -0700

    oops

--- FMBot_Discord/FMBot_Discord/FMBotAdminCommands.cs
+++ FMBot_Discord/FMBot_Discord/FMBotAdminCommands.cs
@@ -584,18 +584,17 @@ namespace FMBot_Discord
         [Command("fmserverlist"), Summary("Displays a list showing information related to every server the bot has joined. - FMBot Admins only")]
         public async Task fmserverlistAsync()
         {
+            var DiscordUser = Context.Message.Author;
+            
             if (FMBotAdminUtil.HasCommandAccess(DiscordUser, 1))
             {
                 var SelfUser = Context.Client as DiscordSocketClient;
-                string description = null;
 
                 foreach (var guild in SelfUser.Guilds)
                 {
-                    description += $"{guild.Name} Users: {guild.Users.Count()} Owner: {guild.Owner.ToString()}\n";
-
                     if (!string.IsNullOrWhiteSpace(description))
                     {
-                        await Context.User.SendMessageAsync(module.Name + "\n" + description);
+                        await Context.User.SendMessageAsync($"{guild.Name} Users: {guild.Users.Count()} Owner: {guild.Owner.ToString()}\n");
                     }
                 }
 
