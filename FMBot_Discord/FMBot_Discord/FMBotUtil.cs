89853105ee4f16c14fbd77bb6713d1783286f5b4
Michael Hart <bitlblox@gmail.com>
2018-03-31 05:45:52 -0700

    added string split method for fmserverlist command

--- FMBot_Discord/FMBot_Discord/FMBotUtil.cs
+++ FMBot_Discord/FMBot_Discord/FMBotUtil.cs
@@ -772,6 +772,14 @@ namespace FMBot_Discord
 
                 return list;
             }
+            
+            public static IEnumerable<string> SplitByLength(this string str, int maxLength)
+            {
+                for (int index = 0; index < str.Length; index += maxLength)
+                {
+                    yield return str.Substring(index, Math.Min(maxLength, str.Length - index));
+                }
+            }
         }
 
         public class FMBotAdminUtil
