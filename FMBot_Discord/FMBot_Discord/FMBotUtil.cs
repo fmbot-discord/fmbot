860da4e8c5390fdb131a8ddec23fbb7f3865fe2b
Michael Hart <bitlblox@gmail.com>
2018-04-17 14:36:30 -0700

    chart text test pt1

--- FMBot_Discord/FMBot_Discord/FMBotUtil.cs
+++ FMBot_Discord/FMBot_Discord/FMBotUtil.cs
@@ -1047,3 +1047,67 @@ public static class StringExtentions
         }
     }
 }
+
+public static class BitmapExtentions
+{
+    public static byte MostDifferent (byte original) 
+    {
+        if(original < 0x80) 
+        {
+            return 0xff;
+        } 
+        else 
+        {
+            return 0x00;
+        }
+    }
+    
+    public static Color MostDifferent (Color original) 
+    {
+        byte r = MostDifferent(original.R);
+        byte g = MostDifferent(original.G);
+        byte b = MostDifferent(original.B);
+        return Color.FromArgb(r,g,b);
+    }
+    
+    public static unsafe Color AverageColor (Bitmap bmp, Rectangle r) 
+    {
+        BitmapData bmd = bmp.LockBits (r, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+        int s = bmd.Stride;
+        int cr = 0;
+        int cg = 0;
+        int cb = 0;
+        int* clr = (int*)(void*)bmd.Scan0;
+        int tmp;
+        int* row = clr;
+        for (int i = 0; i < r.Height; i++) 
+        {
+            int* col = row;
+            for (int j = 0; j < r.Width; j++) 
+            {
+                tmp = *col;
+                cr += (tmp >> 0x10) & 0xff;
+                cg += (tmp >> 0x08) & 0xff;
+                cb += tmp & 0xff;
+                col++;
+            }
+            row += s>>0x02;
+        }
+        int div = r.Width * r.Height;
+        int d2 = div >> 0x01;
+        cr = (cr + d2) / div;
+        cg = (cg + d2) / div;
+        cb = (cb + d2) / div;
+        bmp.UnlockBits (bmd);
+        return Color.FromArgb (cr, cg, cb);
+    }
+    
+    public static void DrawColorString (this Graphics g, Bitmap bmp, string text, Font font, PointF point) 
+    {
+        SizeF sf = g.MeasureString (text, font);
+        Rectangle r = new Rectangle (Point.Truncate (point), Size.Ceiling (sf));
+        r.Intersect (new Rectangle(0,0,bmp.Width,bmp.Height));
+        Color brsh = MostDifferent (AverageColor (bmp, r));
+        g.DrawString (text, font, new SolidBrush (brsh), point);
+    }
+}
