d03154ade5f124673ffc47e8b0a17c4dfce0ad15
Thom <thomkaptein@gmail.com>
2018-12-18 22:26:45 +0100

    #42 include adminservice

---
+++ FMBot_Discord/FMBot_Discord/Services/AdminService.cs
@@ -0,0 +1,92 @@
+﻿using Discord;
+using FMBot.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMBot.Services
+{
+    class AdminService
+    {
+        private FMBotDbContext db = new FMBotDbContext();
+
+        public async Task<bool> HasCommandAccessAsync(IUser discordUser, UserType userType)
+        {
+            string discordUserID = discordUser.Id.ToString();
+
+            User user = await db.Users.FirstOrDefaultAsync(f => f.DiscordUserID == discordUserID);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            switch (user.UserType)
+            {
+                case UserType.Admin:
+                    switch (userType)
+                    {
+                        case UserType.User:
+                            return true;
+                        case UserType.Admin:
+                            return true;
+                        default:
+                            return false;
+                    }
+                case UserType.Owner:
+                    switch (userType)
+                    {
+                        case UserType.User:
+                            return true;
+                        case UserType.Admin:
+                            return true;
+                        default:
+                            return true;
+                    }
+                default:
+                    switch (userType)
+                    {
+                        case UserType.User:
+                            return true;
+                        default:
+                            return false;
+                    }
+            }
+        }
+
+
+        public async Task<bool> SetUserTypeAsync(string discordUserID, UserType userType)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(f => f.DiscordUserID == discordUserID);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            user.UserType = userType;
+
+            db.Entry(user).State = EntityState.Modified;
+
+            await db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public string FormatBytes(long bytes)
+        {
+            string[] Suffix = { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return string.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+    }
+}
