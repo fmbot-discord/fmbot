c465f54ea61ad2b6fa1413f0d0b4d48bb46420e9
Thom <thomkaptein@gmail.com>
2019-10-28 17:55:06 +0100

    Supress ConfigureAwait since it is not needed in Core

--- src/FMBot.Bot/GlobalSuppressions.cs
+++ src/FMBot.Bot/GlobalSuppressions.cs
@@ -1,6 +1,6 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "RCS1090:Call 'ConfigureAwait(false)'.", Justification = "<Pending>", Scope = "member", Target = "~M:FMBot.Bot.Commands.LastFMCommands.fmAsync(System.String)~System.Threading.Tasks.Task")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "RCS1090:Call 'ConfigureAwait(false)'.", Justification = ".NET Core does not need ConfigureAwait", Scope = "namespaceanddescendants", Target = "FMBot.Bot")]
