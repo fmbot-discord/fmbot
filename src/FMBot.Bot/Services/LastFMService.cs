b26b95fe3500ffb3a0c690db9b89202b664cb99d
Thom <thomkaptein@gmail.com>
2019-10-28 21:37:49 +0100

    Refactor LastFMService

--- src/FMBot.Bot/Services/LastFMService.cs
+++ src/FMBot.Bot/Services/LastFMService.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using Dasync.Collections;
 using FMBot.Bot.Configurations;
@@ -22,12 +22,12 @@ namespace FMBot.Bot.Services
 {
     internal class LastFMService
     {
-        public readonly LastfmClient LastfmClient = new LastfmClient(ConfigData.Data.FMKey, ConfigData.Data.FMSecret);
+        private readonly LastfmClient LastFMClient = new LastfmClient(ConfigData.Data.FMKey, ConfigData.Data.FMSecret);
 
         // Last scrobble
         public async Task<LastTrack> GetLastScrobbleAsync(string lastFMUserName)
         {
-            PageResponse<LastTrack> tracks = await LastfmClient.User.GetRecentScrobbles(lastFMUserName, null, 1, 1);
+            var tracks = await this.LastFMClient.User.GetRecentScrobbles(lastFMUserName, null, 1, 1);
             GlobalVars.LastFMApiCalls++;
 
             return tracks.Content[0];
@@ -36,7 +36,7 @@ namespace FMBot.Bot.Services
         // Recent scrobbles
         public async Task<PageResponse<LastTrack>> GetRecentScrobblesAsync(string lastFMUserName, int count = 2)
         {
-            var recentScrobbles = await LastfmClient.User.GetRecentScrobbles(lastFMUserName, null, 1, count);
+            var recentScrobbles = await this.LastFMClient.User.GetRecentScrobbles(lastFMUserName, null, 1, count);
             GlobalVars.LastFMApiCalls++;
 
             return recentScrobbles;
@@ -45,7 +45,7 @@ namespace FMBot.Bot.Services
         // User
         public async Task<LastResponse<LastUser>> GetUserInfoAsync(string lastFMUserName)
         {
-            var user = await LastfmClient.User.GetInfoAsync(lastFMUserName);
+            var user = await this.LastFMClient.User.GetInfoAsync(lastFMUserName);
             GlobalVars.LastFMApiCalls++;
 
             return user;
@@ -54,7 +54,7 @@ namespace FMBot.Bot.Services
         // Album info
         public async Task<LastResponse<LastAlbum>> GetAlbumInfoAsync(string artistName, string albumName)
         {
-            var albumInfo = await LastfmClient.Album.GetInfoAsync(artistName, albumName);
+            var albumInfo = await this.LastFMClient.Album.GetInfoAsync(artistName, albumName);
             GlobalVars.LastFMApiCalls++;
 
             return albumInfo;
@@ -63,16 +63,17 @@ namespace FMBot.Bot.Services
         // Album images
         public async Task<LastImageSet> GetAlbumImagesAsync(string artistName, string albumName)
         {
-            LastResponse<LastAlbum> album = await LastfmClient.Album.GetInfoAsync(artistName, albumName);
+            var album = await this.LastFMClient.Album.GetInfoAsync(artistName, albumName);
             GlobalVars.LastFMApiCalls++;
 
             return album?.Content?.Images;
         }
 
         // Top albums
-        public async Task<PageResponse<LastAlbum>> GetTopAlbumsAsync(string lastFMUserName, LastStatsTimeSpan timespan, int count = 2)
+        public async Task<PageResponse<LastAlbum>> GetTopAlbumsAsync(string lastFMUserName, LastStatsTimeSpan timespan,
+            int count = 2)
         {
-            var topAlbums = await LastfmClient.User.GetTopAlbums(lastFMUserName, timespan, 1, count);
+            var topAlbums = await this.LastFMClient.User.GetTopAlbums(lastFMUserName, timespan, 1, count);
             GlobalVars.LastFMApiCalls++;
 
             return topAlbums;
@@ -81,7 +82,7 @@ namespace FMBot.Bot.Services
         // Artist info
         public async Task<LastResponse<LastArtist>> GetArtistInfoAsync(string artistName)
         {
-            var artistInfo = await LastfmClient.Artist.GetInfoAsync(artistName);
+            var artistInfo = await this.LastFMClient.Artist.GetInfoAsync(artistName);
             GlobalVars.LastFMApiCalls++;
 
             return artistInfo;
@@ -90,18 +91,19 @@ namespace FMBot.Bot.Services
         // Artist info
         public async Task<LastImageSet> GetArtistImageAsync(string artistName)
         {
-            LastResponse<LastArtist> artist = await LastfmClient.Artist.GetInfoAsync(artistName);
+            var artist = await this.LastFMClient.Artist.GetInfoAsync(artistName);
 
-            var artist2 = await LastfmClient.Artist.GetInfoByMbidAsync(artist.Content.Mbid);
+            var artist2 = await this.LastFMClient.Artist.GetInfoByMbidAsync(artist.Content.Mbid);
             GlobalVars.LastFMApiCalls++;
 
             return artist2?.Content?.MainImage;
         }
 
         // Top artists
-        public async Task<PageResponse<LastArtist>> GetTopArtistsAsync(string lastFMUserName, LastStatsTimeSpan timespan, int count = 2)
+        public async Task<PageResponse<LastArtist>> GetTopArtistsAsync(string lastFMUserName,
+            LastStatsTimeSpan timespan, int count = 2)
         {
-            var topArtists = await LastfmClient.User.GetTopArtists(lastFMUserName, timespan, 1, count);
+            var topArtists = await this.LastFMClient.User.GetTopArtists(lastFMUserName, timespan, 1, count);
             GlobalVars.LastFMApiCalls++;
 
             return topArtists;
@@ -110,7 +112,7 @@ namespace FMBot.Bot.Services
         // Check if lastfm user exists
         public async Task<bool> LastFMUserExistsAsync(string lastFMUserName)
         {
-            LastResponse<LastUser> lastFMUser = await LastfmClient.User.GetInfoAsync(lastFMUserName);
+            var lastFMUser = await this.LastFMClient.User.GetInfoAsync(lastFMUserName);
             GlobalVars.LastFMApiCalls++;
 
             return lastFMUser.Success;
@@ -139,11 +141,14 @@ namespace FMBot.Bot.Services
             {
                 return LastStatsTimeSpan.Month;
             }
+
             if (timespan.Equals("yearly") || timespan.Equals("year") || timespan.Equals("y"))
             {
                 return LastStatsTimeSpan.Year;
             }
-            if (timespan.Equals("overall") || timespan.Equals("alltime") || timespan.Equals("o") || timespan.Equals("at") || timespan.Equals("a"))
+
+            if (timespan.Equals("overall") || timespan.Equals("alltime") || timespan.Equals("o") ||
+                timespan.Equals("at") || timespan.Equals("a"))
             {
                 return LastStatsTimeSpan.Overall;
             }
@@ -155,27 +160,29 @@ namespace FMBot.Bot.Services
         {
             try
             {
-                LastStatsTimeSpan timespan = LastStatsTimeSpan.Week;
+                var timespan = LastStatsTimeSpan.Week;
 
                 const string nulltext = "[undefined]";
 
                 if (!Directory.Exists(GlobalVars.CacheFolder))
+                {
                     Directory.CreateDirectory(GlobalVars.CacheFolder);
+                }
 
                 // Album mode
                 await chart.albums.ParallelForEachAsync(async album =>
                 {
-                    string artistName = string.IsNullOrWhiteSpace(album.ArtistName) ? nulltext : album.ArtistName;
-                    string albumName = string.IsNullOrWhiteSpace(album.Name) ? nulltext : album.Name;
+                    var artistName = string.IsNullOrWhiteSpace(album.ArtistName) ? nulltext : album.ArtistName;
+                    var albumName = string.IsNullOrWhiteSpace(album.Name) ? nulltext : album.Name;
 
-                    LastImageSet albumImages = await GetAlbumImagesAsync(artistName, albumName);
+                    var albumImages = await GetAlbumImagesAsync(artistName, albumName);
 
                     Bitmap chartImage;
 
                     if (albumImages?.Large != null)
                     {
-                        string url = albumImages.Large.AbsoluteUri;
-                        string path = Path.GetFileName(url);
+                        var url = albumImages.Large.AbsoluteUri;
+                        var path = Path.GetFileName(url);
 
                         if (File.Exists(GlobalVars.CacheFolder + path))
                         {
@@ -183,19 +190,20 @@ namespace FMBot.Bot.Services
                         }
                         else
                         {
-                            WebRequest request = WebRequest.Create(url);
-                            using WebResponse response = await request.GetResponseAsync();
-                            await using Stream responseStream = response.GetResponseStream();
+                            var request = WebRequest.Create(url);
+                            using var response = await request.GetResponseAsync();
+                            await using var responseStream = response.GetResponseStream();
 
-                            Bitmap bitmap = new Bitmap(responseStream);
+                            var bitmap = new Bitmap(responseStream);
 
                             chartImage = bitmap;
-                            await using MemoryStream memory = new MemoryStream();
-                            await using FileStream fs = new FileStream(GlobalVars.CacheFolder + path, FileMode.Create, FileAccess.ReadWrite);
+                            await using var memory = new MemoryStream();
+                            await using var fs = new FileStream(GlobalVars.CacheFolder + path, FileMode.Create,
+                                FileAccess.ReadWrite);
 
-                            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                            bitmap.Save(memory, ImageFormat.Png);
 
-                            byte[] bytes = memory.ToArray();
+                            var bytes = memory.ToArray();
                             fs.Write(bytes, 0, bytes.Length);
                         }
                     }
@@ -206,7 +214,7 @@ namespace FMBot.Bot.Services
 
                     if (chart.titles)
                     {
-                        Graphics text = Graphics.FromImage(chartImage);
+                        var text = Graphics.FromImage(chartImage);
                         text.DrawColorString(chartImage, artistName, new Font("Arial", 8.0f, FontStyle.Bold),
                             new PointF(2.0f, 2.0f));
                         text.DrawColorString(chartImage, albumName, new Font("Arial", 8.0f, FontStyle.Bold),
@@ -214,7 +222,6 @@ namespace FMBot.Bot.Services
                     }
 
                     chart.images.Add(new ChartImage(chartImage, chart.albums.IndexOf(album)));
-
                 });
             }
             catch (Exception e)
@@ -223,22 +230,25 @@ namespace FMBot.Bot.Services
             }
             finally
             {
-                List<List<Bitmap>> imageList = GlobalVars.splitBitmapList(chart.images.OrderBy(o => o.Index).Select(s => s.Image).ToList(), chart.rows);
+                var imageList =
+                    GlobalVars.splitBitmapList(chart.images.OrderBy(o => o.Index).Select(s => s.Image).ToList(),
+                        chart.rows);
 
-                List<Bitmap> bitmapList = new List<Bitmap>();
-                foreach (List<Bitmap> list in imageList.ToArray())
+                var bitmapList = new List<Bitmap>();
+                foreach (var list in imageList.ToArray())
                 {
                     //combine them into one image
-                    Bitmap stitchedRow = GlobalVars.Combine(list);
+                    var stitchedRow = GlobalVars.Combine(list);
                     bitmapList.Add(stitchedRow);
                 }
 
                 lock (GlobalVars.charts.SyncRoot)
                 {
-                    GlobalVars.charts[GlobalVars.GetChartFileName(chart.DiscordUser.Id)] = GlobalVars.Combine(bitmapList, true);
+                    GlobalVars.charts[GlobalVars.GetChartFileName(chart.DiscordUser.Id)] =
+                        GlobalVars.Combine(bitmapList, true);
                 }
 
-                foreach (Bitmap image in bitmapList.ToArray())
+                foreach (var image in bitmapList.ToArray())
                 {
                     image.Dispose();
                 }
