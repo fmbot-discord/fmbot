0af88b53e9d38164ec4e32568a6bf4830ec1b490
Thom Kaptein <thom@vicompany.nl>
2020-02-15 21:23:59 +0100

    Refactor startupservice

--- src/FMBot.Bot/Services/StartupService.cs
+++ src/FMBot.Bot/Services/StartupService.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
@@ -14,50 +12,54 @@ namespace FMBot.Bot.Services
 {
     public class StartupService
     {
-        private readonly IServiceProvider _provider;
-        private readonly DiscordShardedClient _discord;
         private readonly CommandService _commands;
+        private readonly DiscordShardedClient _discord;
+        private readonly IServiceProvider _provider;
 
         public StartupService(
             IServiceProvider provider,
             DiscordShardedClient discord,
             CommandService commands)
         {
-            _provider = provider;
-            _discord = discord;
-            _commands = commands;
+            this._provider = provider;
+            this._discord = discord;
+            this._commands = commands;
         }
 
         public async Task StartAsync(Logger.Logger logger)
         {
             logger.Log("Starting bot...");
 
-            string discordToken = ConfigData.Data.Token;     // Get the discord token from the config file
+            var discordToken = ConfigData.Data.Token; // Get the discord token from the config file
             if (string.IsNullOrWhiteSpace(discordToken))
+            {
                 throw new Exception("Please enter your bots token into the `/Configs/ConfigData.json` file.");
+            }
 
             await TestLastFMAPI(logger);
 
-            await _discord.LoginAsync(TokenType.Bot, discordToken);     // Login to discord
-            await _discord.StartAsync();                                // Connect to the websocket
+            await this._discord.LoginAsync(TokenType.Bot, discordToken); // Login to discord
+            await this._discord.StartAsync(); // Connect to the websocket
 
-            await _discord.SetGameAsync("Starting bot...");
-            await _discord.SetStatusAsync(UserStatus.DoNotDisturb);
+            await this._discord.SetGameAsync("Starting bot...");
+            await this._discord.SetStatusAsync(UserStatus.DoNotDisturb);
 
-            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);     // Load commands and modules into the command service
+            await this._commands.AddModulesAsync(Assembly.GetEntryAssembly(),
+                this._provider); // Load commands and modules into the command service
         }
 
         private async Task TestLastFMAPI(Logger.Logger logger)
         {
-            LastfmClient fmClient = new LastfmClient(ConfigData.Data.FMKey, ConfigData.Data.FMSecret);
+            var fmClient = new LastfmClient(ConfigData.Data.FMKey, ConfigData.Data.FMSecret);
 
             logger.Log("Checking Last.FM API...");
             var lastFMUser = await fmClient.User.GetInfoAsync("Lastfmsupport");
 
             if (lastFMUser.Status.ToString().Equals("BadApiKey"))
             {
-                logger.LogError("BadLastfmApiKey", "Warning! Invalid API key for Last.FM! Please set the proper API keys in the `/Configs/ConfigData.json`! \n \n" +
-                           "Exiting in 5 seconds...");
+                logger.LogError("BadLastfmApiKey",
+                    "Warning! Invalid API key for Last.FM! Please set the proper API keys in the `/Configs/ConfigData.json`! \n \n" +
+                    "Exiting in 5 seconds...");
 
                 Thread.Sleep(5000);
                 Environment.Exit(0);
