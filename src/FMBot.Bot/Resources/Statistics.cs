ab57960c25ab0994913eba3fb76de56d406a6f35
Thom Kaptein <thom@vicompany.nl>
2020-03-26 18:19:47 +0100

    Rename prometheus names

--- src/FMBot.Bot/Resources/Statistics.cs
+++ src/FMBot.Bot/Resources/Statistics.cs
@@ -7,16 +7,16 @@ namespace FMBot.Bot.Resources
         public static readonly Gauge DiscordServerCount = Metrics
             .CreateGauge("discord_server_count", "Number of servers the bot is in");
 
-        public static readonly Gauge CommandsExecuted = Metrics
-            .CreateGauge("commands_executed", "Amount of commands executed");
-
         public static readonly Gauge LastfmApiCalls = Metrics
             .CreateGauge("lastfm_api_calls", "Amount of last.fm API calls");
 
+        public static readonly Gauge CommandsExecuted = Metrics
+            .CreateGauge("bot_commands_executed", "Amount of commands executed");
+
         public static readonly Gauge RegisteredUsers = Metrics
-            .CreateGauge("registered_users", "Amount of users in the database");
+            .CreateGauge("bot_registered_users", "Amount of users in the database");
 
         public static readonly Gauge RegisteredGuilds = Metrics
-            .CreateGauge("registered_guilds", "Amount of guilds in the database");
+            .CreateGauge("bot_registered_guilds", "Amount of guilds in the database");
     }
 }
