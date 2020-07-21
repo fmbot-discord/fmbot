8156047e61460b9a0be6666e5ee47a34a312ff59
thom kaptein <thomkaptein@gmail.com>
2020-07-21 22:05:22 +0200

    Fixed an issue where artists without spotify images would sometimes bug out

--- src/FMBot.Bot/Services/SpotifyService.cs
+++ src/FMBot.Bot/Services/SpotifyService.cs
@@ -7,6 +7,7 @@ using FMBot.Bot.Extensions;
 using FMBot.LastFM.Domain.Models;
 using FMBot.Persistence.EntityFrameWork;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
@@ -37,83 +38,91 @@ namespace FMBot.Bot.Services
         public async Task<string> GetOrStoreArtistImageAsync(ArtistResponse lastFmArtist, string artistNameBeforeCorrect)
         {
             await using var db = new FMBotDbContext(ConfigData.Data.Database.ConnectionString);
-            var artist = await db.Artists
+            var dbArtist = await db.Artists
                 .AsQueryable()
                 .FirstOrDefaultAsync(f => f.Name.ToLower() == lastFmArtist.Artist.Name.ToLower());
 
             var imageUrlToReturn = "";
-            if (artist == null)
+            try
             {
-                var spotifyArtist = await GetArtistFromSpotify(lastFmArtist.Artist.Name);
-
-                var artistToAdd = new Artist
-                {
-                    Name = lastFmArtist.Artist.Name,
-                    LastFmUrl = lastFmArtist.Artist.Url,
-                    Mbid = lastFmArtist.Artist.Mbid
-                };
-
-                if (spotifyArtist != null)
-                {
-                    artistToAdd.SpotifyId = spotifyArtist.Id;
-
-                    if (spotifyArtist.Images.Any())
-                    {
-                        artistToAdd.SpotifyImageUrl = spotifyArtist.Images.OrderByDescending(o => o.Height).First().Url;
-                        artistToAdd.SpotifyImageDate = DateTime.UtcNow;
-                        imageUrlToReturn = artistToAdd.SpotifyImageUrl;
-                    }
-                }
-
-                if (!string.Equals(artistNameBeforeCorrect, lastFmArtist.Artist.Name, StringComparison.CurrentCultureIgnoreCase))
-                {
-                    artistToAdd.Aliases = new[] { artistNameBeforeCorrect };
-                }
-
-                await db.Artists.AddAsync(artistToAdd);
-                await db.SaveChangesAsync();
-            }
-            else
-            {
-                if (!string.Equals(artistNameBeforeCorrect, lastFmArtist.Artist.Name, StringComparison.CurrentCultureIgnoreCase))
-                {
-                    if (artist.Aliases != null && artist.Aliases.Length > 0)
-                    {
-                        var aliases = artist.Aliases;
-                        Array.Resize(ref aliases, aliases.Length + 1);
-                        aliases[^1] = artistNameBeforeCorrect;
-                        artist.Aliases = aliases;
-                    }
-                    else
-                    {
-                        artist.Aliases = new[] { artistNameBeforeCorrect };
-                    }
-
-                    db.Entry(artist).State = EntityState.Modified;
-                }
-
-                if (artist.SpotifyImageUrl == null || artist.SpotifyImageDate < DateTime.UtcNow.AddMonths(-3))
+                if (dbArtist == null)
                 {
                     var spotifyArtist = await GetArtistFromSpotify(lastFmArtist.Artist.Name);
 
-                    if (spotifyArtist.Images.Any())
+                    var artistToAdd = new Artist
                     {
-                        artist.SpotifyImageUrl = spotifyArtist.Images.OrderByDescending(o => o.Height).First().Url;
-                        artist.SpotifyImageDate = DateTime.UtcNow;
-                        imageUrlToReturn = artist.SpotifyImageUrl;
+                        Name = lastFmArtist.Artist.Name,
+                        LastFmUrl = lastFmArtist.Artist.Url,
+                        Mbid = lastFmArtist.Artist.Mbid
+                    };
 
-                        db.Entry(artist).State = EntityState.Modified;
+                    if (spotifyArtist != null)
+                    {
+                        artistToAdd.SpotifyId = spotifyArtist.Id;
+
+                        if (spotifyArtist.Images.Any())
+                        {
+                            artistToAdd.SpotifyImageUrl = spotifyArtist.Images.OrderByDescending(o => o.Height).First().Url;
+                            artistToAdd.SpotifyImageDate = DateTime.UtcNow;
+                            imageUrlToReturn = artistToAdd.SpotifyImageUrl;
+                        }
                     }
+
+                    if (!string.Equals(artistNameBeforeCorrect, lastFmArtist.Artist.Name, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        artistToAdd.Aliases = new[] { artistNameBeforeCorrect };
+                    }
+
+                    await db.Artists.AddAsync(artistToAdd);
+                    await db.SaveChangesAsync();
                 }
                 else
                 {
-                    imageUrlToReturn = artist.SpotifyImageUrl;
+                    if (!string.Equals(artistNameBeforeCorrect, lastFmArtist.Artist.Name, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        if (dbArtist.Aliases != null && dbArtist.Aliases.Length > 0 && !dbArtist.Aliases.Contains(artistNameBeforeCorrect))
+                        {
+                            var aliases = dbArtist.Aliases;
+                            Array.Resize(ref aliases, aliases.Length + 1);
+                            aliases[^1] = artistNameBeforeCorrect;
+                            dbArtist.Aliases = aliases;
+                        }
+                        else
+                        {
+                            dbArtist.Aliases = new[] { artistNameBeforeCorrect };
+                        }
+
+                        db.Entry(dbArtist).State = EntityState.Modified;
+                    }
+
+                    if (dbArtist.SpotifyImageUrl == null || dbArtist.SpotifyImageDate < DateTime.UtcNow.AddMonths(-3))
+                    {
+                        var spotifyArtist = await GetArtistFromSpotify(lastFmArtist.Artist.Name);
+
+                        if (spotifyArtist != null && spotifyArtist.Images.Any())
+                        {
+                            dbArtist.SpotifyImageUrl = spotifyArtist.Images.OrderByDescending(o => o.Height).First().Url;
+                            dbArtist.SpotifyImageDate = DateTime.UtcNow;
+                            imageUrlToReturn = dbArtist.SpotifyImageUrl;
+
+                            db.Entry(dbArtist).State = EntityState.Modified;
+                        }
+                    }
+                    else
+                    {
+                        imageUrlToReturn = dbArtist.SpotifyImageUrl;
+                    }
+
+                    await db.SaveChangesAsync();
                 }
 
-                await db.SaveChangesAsync();
+                return !string.IsNullOrEmpty(imageUrlToReturn) ? imageUrlToReturn : null;
             }
-
-            return !string.IsNullOrEmpty(imageUrlToReturn) ? imageUrlToReturn : null;
+            catch (Exception e)
+            {
+                Log.Error(e, "Something went wrong while retrieving artist image");
+                return null;
+            }
         }
 
         private static async Task<FullArtist> GetArtistFromSpotify(string artistName)
@@ -138,7 +147,7 @@ namespace FMBot.Bot.Services
                     .OrderByDescending(o => o.Popularity)
                     .FirstOrDefault(w => w.Name.ToLower() == artistName.ToLower());
 
-                if (spotifyArtist != null)
+                if (spotifyArtist != null && !spotifyArtist.HasError())
                 {
                     return spotifyArtist;
                 }
