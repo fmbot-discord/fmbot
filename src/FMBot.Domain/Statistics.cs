913b177c7844c6fc6bdb92b6b0ed20516f380381
thomkaptein <thomkaptein@gmail.com>
2020-12-22 17:59:26 +0100

    Change prometheus types

--- src/FMBot.Domain/Statistics.cs
+++ src/FMBot.Domain/Statistics.cs
@@ -8,27 +8,28 @@ namespace FMBot.Domain
             .CreateGauge("discord_server_count", "Total count of all servers the bot is in");
 
 
-        public static readonly Gauge LastfmApiCalls = Metrics
-            .CreateGauge("lastfm_api_calls", "Amount of last.fm API calls");
+        public static readonly Counter LastfmApiCalls = Metrics
+            .CreateCounter("lastfm_api_calls", "Amount of last.fm API calls");
 
-        public static readonly Gauge LastfmAuthorizedApiCalls = Metrics
-            .CreateGauge("lastfm_authorized_api_calls", "Amount of authorized last.fm API calls");
+        public static readonly Counter LastfmAuthorizedApiCalls = Metrics
+            .CreateCounter("lastfm_authorized_api_calls", "Amount of authorized last.fm API calls");
 
-        public static readonly Gauge LastfmImageCalls = Metrics
-            .CreateGauge("lastfm_image_cdn_calls", "Amount of calls to the last.fm image cdn");
+        public static readonly Counter LastfmImageCalls = Metrics
+            .CreateCounter("lastfm_image_cdn_calls", "Amount of calls to the last.fm image cdn");
 
-        public static readonly Gauge LastfmCachedImageCalls = Metrics
-            .CreateGauge("lastfm_cached_image_cdn_calls", "Amount of calls locally cached to last.fm images");
+        public static readonly Counter LastfmCachedImageCalls = Metrics
+            .CreateCounter("lastfm_cached_image_cdn_calls", "Amount of calls locally cached to last.fm images");
 
         public static readonly Histogram LastfmApiResponseTime = Metrics
             .CreateHistogram("lastfm_api_response_time", "Histogram of Last.fm API response time");
 
 
-        public static readonly Gauge CommandsExecuted = Metrics
-            .CreateGauge("bot_commands_executed", "Amount of commands executed");
+        public static readonly Counter CommandsExecuted = Metrics
+            .CreateCounter("bot_commands_executed", "Amount of commands executed");
 
-        public static readonly Gauge SlashCommandsExecuted = Metrics
-            .CreateGauge("bot_slash_commands_executed", "Amount of slash commands executed");
+        public static readonly Counter SlashCommandsExecuted = Metrics
+            .CreateCounter("bot_slash_commands_executed", "Amount of slash commands executed");
+        
 
         public static readonly Gauge RegisteredUserCount = Metrics
             .CreateGauge("bot_registered_users_count", "Total count of all users in the database");
@@ -38,11 +39,12 @@ namespace FMBot.Domain
 
         public static readonly Gauge RegisteredGuildCount = Metrics
             .CreateGauge("bot_registered_guilds_count", "Total count of all guilds in the database");
+        
 
-        public static readonly Gauge IndexedUsers = Metrics
-            .CreateGauge("bot_indexed_users", "Amount of indexed users");
+        public static readonly Counter IndexedUsers = Metrics
+            .CreateCounter("bot_indexed_users", "Amount of indexed users");
 
-        public static readonly Gauge UpdatedUsers = Metrics
-            .CreateGauge("bot_updated_users", "Amount of updated users");
+        public static readonly Counter UpdatedUsers = Metrics
+            .CreateCounter("bot_updated_users", "Amount of updated users");
     }
 }
