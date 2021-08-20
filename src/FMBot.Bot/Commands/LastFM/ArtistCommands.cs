ec432d4ddf2dd51395e67f36aac19a7243da928b
thomkaptein <thomkaptein@gmail.com>
2021-08-20 17:41:49 +0200

    Improve taste errors

--- src/FMBot.Bot/Commands/LastFM/ArtistCommands.cs
+++ src/FMBot.Bot/Commands/LastFM/ArtistCommands.cs
@@ -663,25 +663,23 @@ namespace FMBot.Bot.Commands.LastFM
 
                 if (lastfmToCompare == null)
                 {
-                    var errorReply = $"Error while attempting taste comparison: \n\n" +
-                                     $"Please enter a valid user to compare your top artists to.\n" +
-                                     $"Example: `{prfx}taste lastfmusername` or `{prfx}taste @user`.\n" +
-                                     $"Make sure the user is registered in .fmbot or has a public Last.fm profile.";
-
-
-                    await ReplyAsync(errorReply);
+                    this._embed.WithDescription($"Please enter a Last.fm username or mention someone to compare yourself to.\n" +
+                                                $"Examples:\n" +
+                                                $"- `{prfx}taste fm-bot`\n" +
+                                                $"- `{prfx}taste @.fmbot`");
+                    await this.Context.Channel.SendMessageAsync("", false, this._embed.Build());
 
                     this.Context.LogCommandUsed(CommandResponse.WrongInput);
                     return;
                 }
                 if (lastfmToCompare.ToLower() == userSettings.UserNameLastFM.ToLower())
                 {
-                    var errorReply =
-                        $"Error while attempting taste comparison: \n\n" +
-                        $"You can't compare your own taste with yourself. For viewing your top artists, use `{prfx}topartists`\n" +
-                        $"Please enter a different last.fm username or mention another user.";
-
-                    await ReplyAsync(errorReply);
+                    this._embed.WithDescription($"You can't compare your own taste with yourself. For viewing your top artists, use `{prfx}topartists`.\n\n" +
+                                                $"Please enter a Last.fm username or mention someone to compare yourself to.\n" +
+                                                $"Examples:\n" +
+                                                $"- `{prfx}taste fm-bot`\n" +
+                                                $"- `{prfx}taste @.fmbot`");
+                    await this.Context.Channel.SendMessageAsync("", false, this._embed.Build());
 
                     this.Context.LogCommandUsed(CommandResponse.WrongInput);
                     return;
