17070a5050fe3ba35d4dd2fbd8be8bffef0d69a8
thomkaptein <thomkaptein@gmail.com>
2021-12-09 18:25:06 +0100

    fix dupe featured postings

--- src/FMBot.Bot/Services/TimerService.cs
+++ src/FMBot.Bot/Services/TimerService.cs
@@ -95,14 +95,13 @@ namespace FMBot.Bot.Services
 
                             var botType = BotTypeExtension.GetBotType(client.CurrentUser.Id);
                             await this._webhookService.PostFeatured(newFeatured, client);
+                            await this._featuredService.SetFeatured(newFeatured);
                             await this._webhookService.SendFeaturedWebhooks(botType, newFeatured);
 
                             if (newFeatured.FeaturedMode == FeaturedMode.RecentPlays)
                             {
                                 await this._featuredService.ScrobbleTrack(client.CurrentUser.Id, newFeatured);
                             }
-
-                            await this._featuredService.SetFeatured(newFeatured);
                         }
 
                         this._currentFeatured = await this._featuredService.GetFeaturedForDateTime(DateTime.UtcNow);
