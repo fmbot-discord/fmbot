c1986b419805cb56b4c7a533b8e8f15de84cf90a
thomkaptein <thomkaptein@gmail.com>
2022-01-06 21:42:34 +0100

    month in previous year if it has not happened yet

--- src/FMBot.Bot/Services/SettingService.cs
+++ src/FMBot.Bot/Services/SettingService.cs
@@ -48,6 +48,12 @@ namespace FMBot.Bot.Services
                     month.GetValueOrDefault(1),
                     1);
 
+                if (month.HasValue && month.Value > DateTime.UtcNow.Month && !year.HasValue)
+                {
+                    settingsModel.StartDateTime = settingsModel.StartDateTime.Value.AddYears(-1);
+                    year = settingsModel.StartDateTime.Value.Year;
+                }
+
                 if (year.HasValue && !month.HasValue)
                 {
                     settingsModel.Description = $"{year}";
