f660d86a7160e816b9eef1b62fb37e56b2bec968
thomkaptein <thomkaptein@gmail.com>
2022-01-06 22:12:42 +0100

    improve server index experience, index on guild join

--- src/FMBot.Bot/Handlers/CommandHandler.cs
+++ src/FMBot.Bot/Handlers/CommandHandler.cs
@@ -243,8 +243,8 @@ public class CommandHandler
             if (lastIndex == null)
             {
                 var embed = new EmbedBuilder();
-                embed.WithDescription("To use .fmbot commands with server-wide statistics you need to index the server first.\n\n" +
-                                      $"Please run `{prfx}index` to index this server.\n" +
+                embed.WithDescription("To use .fmbot commands with server-wide statistics you need to create a memberlist cache first.\n\n" +
+                                      $"Please run `{prfx}refreshmembers` to create this.\n" +
                                       $"Note that this can take some time on large servers.");
                 await context.Channel.SendMessageAsync("", false, embed.Build());
                 context.LogCommandUsed(CommandResponse.IndexRequired);
@@ -253,7 +253,7 @@ public class CommandHandler
             if (lastIndex < DateTime.UtcNow.AddDays(-120))
             {
                 var embed = new EmbedBuilder();
-                embed.WithDescription("Server index data is out of date, it was last updated over 100 days ago.\n" +
+                embed.WithDescription("Server index data is out of date, it was last updated over 120 days ago.\n" +
                                       $"Please run `{prfx}index` to re-index this server.");
                 await context.Channel.SendMessageAsync("", false, embed.Build());
                 context.LogCommandUsed(CommandResponse.IndexRequired);
