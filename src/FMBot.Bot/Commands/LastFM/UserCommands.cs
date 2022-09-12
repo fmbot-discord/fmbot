96fd49aa633eb092352060106aa343a777389dc0
thomkaptein <thomkaptein@gmail.com>
2022-09-12 17:58:24 +0200

    add one more alias

--- src/FMBot.Bot/Commands/LastFM/UserCommands.cs
+++ src/FMBot.Bot/Commands/LastFM/UserCommands.cs
@@ -199,7 +199,7 @@ namespace FMBot.Bot.Commands.LastFM
         [Command("featured", RunMode = RunMode.Async)]
         [Summary("Displays the currently picked feature and the user.\n\n" +
                  "This command will also show something special if the user is in your server")]
-        [Alias("featuredavatar", "featureduser", "featuredalbum", "avatar", "ftrd", "feat")]
+        [Alias("featuredavatar", "featureduser", "featuredalbum", "avatar", "ftrd", "ftd", "feat")]
         [CommandCategories(CommandCategory.Other)]
         public async Task FeaturedAsync([Remainder] string options = null)
         {
