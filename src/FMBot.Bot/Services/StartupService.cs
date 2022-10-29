083b42e4bde198b1ba8288c20732a8321f6bc1fc
Thom <thomkaptein@gmail.com>
2022-10-29 17:02:16 +0200

    decrease shard timeout

--- src/FMBot.Bot/Services/StartupService.cs
+++ src/FMBot.Bot/Services/StartupService.cs
@@ -119,7 +119,7 @@ public class StartupService
                 Assembly.GetEntryAssembly(),
                 this._provider);
 
-        var shardTimeOut = 4500;
+        var shardTimeOut = 4300;
         foreach (var shard in this._client.Shards)
         {
             Log.Information("ShardStartConnection: shard {shardId}", shard.ShardId);
