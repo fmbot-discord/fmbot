bba97833f93fa82f80bc4f17acc07a6435db3fb6
thomkaptein <thomkaptein@gmail.com>
2022-11-15 23:13:34 +0100

    add newline

--- src/FMBot.Bot/Builders/StaticBuilders.cs
+++ src/FMBot.Bot/Builders/StaticBuilders.cs
@@ -55,7 +55,7 @@ public class StaticBuilders
         response.Embed.AddField("Get more stats",
             "- See first listen dates for artists/albums/tracks\n" +
             "- Expanded `stats` command with overall history\n" +
-            "- Extra page on `year` overview" +
+            "- Extra page on `year` overview\n" +
             "- More coming soon");
 
         response.Embed.AddField("Flex your support",
