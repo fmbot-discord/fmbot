d23f47c70e5a06f92118453dddb6b09bb012d1fe
umbreon22 <umbreon22@users.noreply.github.com>
2023-02-27 12:47:00 -0800

    Added MusicBot implementations for supported bots

---
+++ src/FMBot.Bot/Models/MusicBot/HydraMusicBot.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Discord;
+using Discord.WebSocket;
+
+namespace FMBot.Bot.Models.MusicBot;
+
+internal class HydraMusicBot : MusicBot
+{
+    public HydraMusicBot() : base("Hydra", false)
+    {
+    }
+
+    public override bool ShouldIgnoreMessage(IUserMessage msg)
+    {
+        return msg.Embeds == null ||
+               !msg.Embeds.Any() ||
+               msg.Embeds.Any(a => a.Title == null) ||
+               (msg.Embeds.Any(a => a.Title != "Now playing") && msg.Embeds.Any(a => a.Title != "Speelt nu")) ||
+               msg.Embeds.Any(a => a.Description == null);
+    }
+
+    public override string GetTrackQuery(IUserMessage msg)
+    {
+        return msg.Embeds.First().Description;
+    }
+}
