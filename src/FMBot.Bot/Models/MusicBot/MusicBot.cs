30e6b521659d51d73e043e797ed6ab9fa522fb0f
umbreon22 <umbreon22@users.noreply.github.com>
2023-02-27 12:45:48 -0800

    Added MusicBot model

---
+++ src/FMBot.Bot/Models/MusicBot/MusicBot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Discord;
+using Discord.WebSocket;
+
+namespace FMBot.Bot.Models.MusicBot;
+
+public abstract class MusicBot
+{
+    public string Name { get; }
+    public bool PossiblyIncludesLinks { get; }
+
+    public bool SkipUploaderName { get; }
+
+    public static IEnumerable<MusicBot> SupportedBots { get; } = new List<MusicBot>
+    {
+        new CakeyBotMusicBot(),
+        new SoundCloudMusicBot(),
+        new HydraMusicBot()
+    };
+
+    protected MusicBot(string name, bool possiblyIncludesLinks = true, bool skipUploaderName = false)
+    {
+        this.Name = name;
+        this.PossiblyIncludesLinks = possiblyIncludesLinks;
+        this.SkipUploaderName = skipUploaderName;
+    }
+
+    public bool IsAuthor(SocketUser user)
+    {
+        return user?.Username?.StartsWith(this.Name) ?? false;
+    }
+
+    public abstract bool ShouldIgnoreMessage(IUserMessage msg);
+
+    public abstract string GetTrackQuery(IUserMessage msg);
+}
