b5028eb05610b255170600ff080da8bf7a8649c3
thomkaptein <thomkaptein@gmail.com>
2023-04-11 00:47:13 +0200

    increase metric timer delay

--- src/FMBot.Bot/Services/TimerService.cs
+++ src/FMBot.Bot/Services/TimerService.cs
@@ -276,7 +276,7 @@ public class TimerService
 
             },
             null,
-            TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(30),
             TimeSpan.FromMinutes(2));
 
         this._shardReconnectTimer = new Timer(async _ =>
