58ccdf506430bf72a34336887a92547e5ea760ca
th0mk <thomkaptein@gmail.com>
2023-07-14 18:15:31 +0200

    fix text

--- src/FMBot.Bot/Builders/UserBuilder.cs
+++ src/FMBot.Bot/Builders/UserBuilder.cs
@@ -1029,7 +1029,7 @@ public class UserBuilder
         if (!hasImported)
         {
             embedDescription.AppendLine();
-            embedDescription.AppendLine("Run the `/import spotify` command to get started with imports, or read the guide (todo link). " +
+            embedDescription.AppendLine("Run the `/import spotify` command to get started with imports. " +
                                         "After importing you'll be able to change these settings.");
         }
 
