c6fc8a7a8a05bcadf6cd6e336b7d77d19c0603b0
thomkaptein <thomkaptein@gmail.com>
2023-07-18 17:35:33 +0200

    adjust startup

--- src/FMBot.Bot/Services/StartupService.cs
+++ src/FMBot.Bot/Services/StartupService.cs
@@ -138,7 +138,7 @@ public class StartupService
                 Assembly.GetEntryAssembly(),
                 this._provider);
 
-        var shardTimeOut = 1000;
+        var shardTimeOut = 900;
         foreach (var shard in this._client.Shards)
         {
             Log.Information("ShardStartConnection: shard {shardId} - timeout {shardTimeout}", shard.ShardId, shardTimeOut);
@@ -146,9 +146,9 @@ public class StartupService
             await Task.Delay(shardTimeOut);
             shardTimeOut += 5;
 
-            if (shardTimeOut > 2000)
+            if (shardTimeOut > 1800)
             {
-                shardTimeOut = 2000;
+                shardTimeOut = 1800;
             }
         }
 
