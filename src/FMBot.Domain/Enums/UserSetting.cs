2342933c7db593654a4adaba8b9c061435f7424f
thomkaptein <thomkaptein@gmail.com>
2023-08-03 13:49:37 +0200

    adjust order

--- src/FMBot.Domain/Enums/UserSetting.cs
+++ src/FMBot.Domain/Enums/UserSetting.cs
@@ -10,14 +10,14 @@ public enum UserSetting
     FmMode = 3,
     [Option("WhoKnows mode", "Set the default WhoKnows response type")]
     WkMode = 4,
-    [Option("Music bot scrobbling", "Toggle automatic scrobbling other music bots")]
-    BotScrobbling = 5,
     [Option("Out of sync", "Info on what to do when Spotify and Last.fm are out of sync")]
     OutOfSync = 6,
     [Option("Spotify import ⭐", "Manage your Spotify imports")]
     SpotifyImport = 10,
     [Option("User reactions ⭐", "Set personal automated emoji reactions")]
     UserReactions = 11,
+    [Option("Music bot scrobbling", "Toggle automatic scrobbling other music bots")]
+    BotScrobbling = 20,
     [Option("Delete account", "Delete your .fmbot data and account")]
     DeleteAccount = 30
 }
