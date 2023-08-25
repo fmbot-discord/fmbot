fde675c231a93c2b62d628a1471f204d06f200e5
thomkaptein <thomkaptein@gmail.com>
2023-08-25 14:08:06 +0200

    fix nsfw album in some cases

--- src/FMBot.Bot/Services/CensorService.cs
+++ src/FMBot.Bot/Services/CensorService.cs
@@ -122,8 +122,6 @@ public class CensorService
             {
                 return CensorResult.NotSafe;
             }
-
-            return CensorResult.Safe;
         }
 
         if (censoredMusic
