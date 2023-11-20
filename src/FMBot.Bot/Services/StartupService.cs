7a1764a6e8c4b5e61d2b2885a9c3f487b7f809c9
thomkaptein <thomkaptein@gmail.com>
2023-11-20 13:30:40 +0100

    even slower startup

--- src/FMBot.Bot/Services/StartupService.cs
+++ src/FMBot.Bot/Services/StartupService.cs
@@ -140,7 +140,7 @@ public class StartupService
         Log.Information("Preparing cache folder");
         PrepareCacheFolder();
 
-        var shardTimeOut = 1400;
+        var shardTimeOut = 1800;
         foreach (var shard in this._client.Shards)
         {
             Log.Information("ShardStartConnection: shard {shardId} - timeout {shardTimeout}", shard.ShardId, shardTimeOut);
@@ -148,9 +148,9 @@ public class StartupService
             await Task.Delay(shardTimeOut);
             shardTimeOut += 10;
 
-            if (shardTimeOut > 2200)
+            if (shardTimeOut > 3000)
             {
-                shardTimeOut = 2200;
+                shardTimeOut = 3000;
             }
         }
 
