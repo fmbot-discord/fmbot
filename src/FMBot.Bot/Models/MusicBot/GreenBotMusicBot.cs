f2e35c7f4156635acea454295f8b8e5f2b187d32
Sean Sattler <sean@sattler.dev>
2024-01-15 20:09:19 +0100

    Create GreenBotMusicBot.cs
    Added support for green-bot.app

---
+++ src/FMBot.Bot/Models/MusicBot/GreenBotMusicBot.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using Discord;
+using Discord.WebSocket;
+
+namespace FMBot.Bot.Models.MusicBot;
+
+internal class GreenBotMusicBot : MusicBot
+{
+    private const string NowPlayingStatus = "- Now Playing";
+    public override bool ShouldIgnoreMessage(IUserMessage msg)
+    {
+        var embed = msg.Embeds.FirstOrDefault();
+        if (embed == null)
+        {
+            return true;
+        }
+
+        var authorName = embed.Author?.Name ?? string.Empty;
+        return !authorName.Contains(NowPlayingStatus);
+    }
+
+    public override string GetTrackQuery(IUserMessage msg)
+    {
+        var embed = msg.Embeds.FirstOrDefault();
+        if (embed == null)
+        {
+            return string.Empty;
+        }
+
+        var description = embed.Description ?? string.Empty;
+        var songDetails = description.Split(new string[] { " by " }, StringSplitOptions.None);
+
+        if (
+            songDetails.Length < 2)
+        {
+            return string.Empty; 
+        }
+
+        var songName = songDetails[0].Trim();
+        var author = songDetails[1].Trim();
+
+        return $"{songName} - {author}";
+    }
+}
