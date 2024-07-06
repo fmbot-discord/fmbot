493b859c8e5e1f8c9470817d37a83c77409a5bbc
thomkaptein <thomkaptein@gmail.com>
2024-07-06 15:12:39 +0200

    improve am error handling

--- src/FMBot.AppleMusic/AppleMusicApi.cs
+++ src/FMBot.AppleMusic/AppleMusicApi.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using FMBot.AppleMusic.Converters;
 using FMBot.AppleMusic.Models;
+using Serilog;
 
 namespace FMBot.AppleMusic;
 
@@ -30,51 +31,74 @@ public class AppleMusicApi
     public async Task<AmArtist> GetArtistAsync(string artistId)
     {
         var response = await this._client.GetAsync($"artists/{artistId}");
-        response.EnsureSuccessStatusCode();
-
         var requestBody = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("AppleMusicApi: Bad HTTP status code in GetArtistAsync - {statusCode} - {requestBody}", response.StatusCode, requestBody);
+            return null;
+        }
+        
         return JsonSerializer.Deserialize<AmArtist>(requestBody, GetJsonSerializerOptions());
     }
 
     public async Task<AmSong> GetSongAsync(string songId)
     {
         var response = await this._client.GetAsync($"songs/{songId}");
-        response.EnsureSuccessStatusCode();
-
         var requestBody = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("AppleMusicApi: Bad HTTP status code in GetSongAsync - {statusCode} - {requestBody}", response.StatusCode, requestBody);
+            return null;
+        }
+        
         return JsonSerializer.Deserialize<AmSong>(requestBody, GetJsonSerializerOptions());
     }
 
     public async Task<List<AmData<AmArtistAttributes>>> SearchArtistAsync(string searchQuery)
     {
         var response = await this._client.GetAsync($"search?types=artists&term={searchQuery}");
-        response.EnsureSuccessStatusCode();
-
         var requestBody = await response.Content.ReadAsStringAsync();
-        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
 
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("AppleMusicApi: Bad HTTP status code in SearchArtistAsync - {statusCode} - {requestBody}", response.StatusCode, requestBody);
+            return null;
+        }
+
+        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
         return results.Results.Artists?.Data;
     }
 
     public async Task<List<AmData<AmAlbumAttributes>>> SearchAlbumAsync(string searchQuery)
     {
         var response = await this._client.GetAsync($"search?types=albums&term={searchQuery}");
-        response.EnsureSuccessStatusCode();
-
         var requestBody = await response.Content.ReadAsStringAsync();
-        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
 
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("AppleMusicApi: Bad HTTP status code in SearchAlbumAsync - {statusCode} - {requestBody}", response.StatusCode, requestBody);
+            return null;
+        }
+        
+        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
         return results.Results.Albums?.Data;
     }
 
     public async Task<List<AmData<AmSongAttributes>>> SearchSongAsync(string searchQuery)
     {
         var response = await this._client.GetAsync($"search?types=songs&term={searchQuery}");
-        response.EnsureSuccessStatusCode();
-
+        
         var requestBody = await response.Content.ReadAsStringAsync();
-        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
 
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("AppleMusicApi: Bad HTTP status code in SearchSongAsync - {statusCode} - {requestBody}", response.StatusCode, requestBody);
+            return null;
+        }
+
+        var results = JsonSerializer.Deserialize<AmSearchResult>(requestBody, GetJsonSerializerOptions());
         return results.Results.Songs?.Data;
     }
 }
