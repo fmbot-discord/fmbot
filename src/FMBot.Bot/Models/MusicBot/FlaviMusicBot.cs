706d17cc9828feeed50bb45e792f76f184148bcf
Thom <thomkaptein@gmail.com>
2025-07-29 13:38:15 +0200

    fix flavi bot scrobbling

--- src/FMBot.Bot/Models/MusicBot/FlaviMusicBot.cs
+++ src/FMBot.Bot/Models/MusicBot/FlaviMusicBot.cs
@@ -13,29 +13,59 @@ internal class FlaviMusicBot : MusicBot
 
     public override bool ShouldIgnoreMessage(IUserMessage msg)
     {
-        if (msg.Embeds.Count != 1)
+        var container = msg.Components.FirstOrDefault(c => c.Type == ComponentType.Container);
+        if (container is not ContainerComponent containerComponent)
         {
             return true;
         }
 
-        var title = msg.Embeds.First().Author?.Name;
-        return string.IsNullOrEmpty(title) || !title.Contains(NowPlaying);
+        var section = containerComponent.Components.FirstOrDefault(c => c.Type == ComponentType.Section);
+        if (section is not SectionComponent sectionComponent)
+        {
+            return true;
+        }
+
+        var text = sectionComponent.Components.FirstOrDefault(f => f.Type == ComponentType.TextDisplay);
+        if (text is not TextDisplayComponent textComponent)
+        {
+            return true;
+        }
+
+        return string.IsNullOrEmpty(textComponent?.Content) || !textComponent.Content.Contains(NowPlaying);
     }
 
     /**
      * Example:
-     * **[Johnny Hallyday - Allumer le feu](https://deezer.com/track/921379)** - `05:29`
+     * ### **[Michael Jackson - Billie Jean](https://open.spotify.com/track/7J1uxwnxfQLu4APicE5Rnj)** - `04:54`
      *
      */
     public override string GetTrackQuery(IUserMessage msg)
     {
-        var track = msg.Embeds.First().Description;
+        var container = msg.Components.FirstOrDefault(c => c.Type == ComponentType.Container);
+        if (container is not ContainerComponent containerComponent)
+        {
+            return string.Empty;
+        }
+
+        var section = containerComponent.Components.FirstOrDefault(c => c.Type == ComponentType.Section);
+        if (section is not SectionComponent sectionComponent)
+        {
+            return string.Empty;
+        }
+
+        var text = sectionComponent.Components.Skip(1).FirstOrDefault(f => f.Type == ComponentType.TextDisplay);
+        if (text is not TextDisplayComponent textComponent)
+        {
+            return string.Empty;
+        }
+
+        var track = textComponent.Content;
 
         if (track == null)
         {
             return string.Empty;
         }
 
-        return track;
+        return track.Replace("### ", "");
     }
 }
