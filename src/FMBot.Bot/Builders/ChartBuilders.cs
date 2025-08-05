461bbf4c15e65f8505b9c41d74caff0a2b284ee1
Thom <thomkaptein@gmail.com>
2025-08-05 12:26:21 +0200

    test code

--- src/FMBot.Bot/Builders/ChartBuilders.cs
+++ src/FMBot.Bot/Builders/ChartBuilders.cs
@@ -211,7 +211,6 @@ public class ChartBuilders
 
         var supporter =
             await this._supporterService.GetRandomSupporter(context.DiscordGuild, context.ContextUser.UserType);
-        supporter = "test123";
         ChartService.AddSettingsToDescription(chartSettings, embedDescription, supporter, context.Prefix);
         if (supporter != null)
         {
