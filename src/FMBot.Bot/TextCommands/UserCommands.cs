08321d6424d7dd9686b9df8389a18294404c189a
Thom <thomkaptein@gmail.com>
2025-09-16 23:11:39 +0200

    fix

--- src/FMBot.Bot/TextCommands/UserCommands.cs
+++ src/FMBot.Bot/TextCommands/UserCommands.cs
@@ -653,11 +653,13 @@ public class UserCommands : BaseCommandModule
 
     [Command("shortcuts")]
     [Alias("shortcut", "sc", "scs")]
-    [Summary("Allows you to set text command shortcuts. Input is converted to your output while parameters are preserved. Shortcuts still need to use the normal text command prefix.")]
-    [SupporterExclusive("Shortcuts are a feature exclusive to Supporters. Shortcuts are stored in-memory across all bot instances to ensure command handling remains fast, so there is limited availability.")]
+    [Summary(
+        "Allows you to set text command shortcuts. Input is converted to your output while parameters are preserved. Shortcuts still need to use the normal text command prefix.")]
+    [SupporterExclusive(
+        "Shortcuts are a feature exclusive to Supporters. Shortcuts are stored in-memory across all bot instances to ensure command handling remains fast, so there is limited availability.")]
     [Examples("yo` > `fm textoneline", "progress` > `chart 5x5 2025 skip")]
     [UsernameSetRequired]
-    public async Task ShortcutsAsync()
+    public async Task ShortcutsAsync([Remainder] string _ = null)
     {
         try
         {
