d32c9516012db197e45a5a3e3cabf65baa5732ff
Thom <thomkaptein@gmail.com>
2026-03-14 15:42:07 +0100

    remove some unused values

---
+++ src/FMBot.Persistence.EntityFrameWork/Migrations/20260314144049_CleanUnusedValues.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMBot.Persistence.EntityFrameWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanUnusedValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fm_embed_type",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "fm_footer_options",
+                table: "users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "fm_embed_type",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "fm_footer_options",
+                table: "users",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
