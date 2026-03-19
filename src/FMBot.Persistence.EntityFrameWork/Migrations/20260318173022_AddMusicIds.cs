5c4a7b07e018e0d929b903540797070c488a6d25
Thom <thomkaptein@gmail.com>
2026-03-19 14:41:55 +0100

    add unique to migration

--- src/FMBot.Persistence.EntityFrameWork/Migrations/20260318173022_AddMusicIds.cs
+++ src/FMBot.Persistence.EntityFrameWork/Migrations/20260318173022_AddMusicIds.cs
@@ -45,11 +45,39 @@ namespace FMBot.Persistence.EntityFrameWork.Migrations
                 table: "user_albums",
                 type: "integer",
                 nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_artists_name_unique",
+                table: "artists",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_albums_artist_name_name_unique",
+                table: "albums",
+                columns: new[] { "artist_name", "name" },
+                unique: true);
+
+            migrationBuilder.Sql(
+                @"CREATE UNIQUE INDEX ix_tracks_artist_name_name_album_unique
+                  ON tracks (artist_name, name, COALESCE(album_name, ''));");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropIndex(
+                name: "ix_artists_name_unique",
+                table: "artists");
+
+            migrationBuilder.DropIndex(
+                name: "ix_albums_artist_name_name_unique",
+                table: "albums");
+
+            migrationBuilder.DropIndex(
+                name: "ix_tracks_artist_name_name_album_unique",
+                table: "tracks");
+
             migrationBuilder.DropColumn(
                 name: "track_id",
                 table: "user_tracks");
