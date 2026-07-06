701664f4c2e3926e4886d6798d671026dcfe320f
th0mk <thomkaptein@gmail.com>
2026-07-06 17:18:15 +0200

    add migration

---
+++ src/FMBot.Persistence.EntityFrameWork/Migrations/20260706151607_AddAlbumGenres.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FMBot.Persistence.EntityFrameWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlbumGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "album_genres",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    album_id = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "citext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_album_genres", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_album_genres_albums_album_id",
+                        column: x => x.album_id,
+                        principalTable: "albums",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_album_genres_album_id_name",
+                table: "album_genres",
+                columns: new[] { "album_id", "name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "album_genres");
+        }
+    }
+}
