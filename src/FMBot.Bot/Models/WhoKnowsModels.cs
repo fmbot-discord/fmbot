3e2831fa7c4b7716603f6ff094213c160be90f64
Thom <thomkaptein@gmail.com>
2026-07-14 00:05:00 +0200

    improve checkbotted

--- src/FMBot.Bot/Models/WhoKnowsModels.cs
+++ src/FMBot.Bot/Models/WhoKnowsModels.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Security.AccessControl;
+using System.Collections.Generic;
 using FMBot.Domain.Models;
 
 namespace FMBot.Bot.Models;
@@ -21,13 +21,50 @@ public class GlobalFilterCandidate
     public DateTime LastPlay { get; set; }
 }
 
+public class BottedCheckStats
+{
+    public int PlaysMonth { get; set; }
+
+    public int PlaysWeek { get; set; }
+
+    public long MsMonth { get; set; }
+
+    public long MsWeek { get; set; }
+
+    public int UnknownDurationPlays { get; set; }
+
+    public int DuplicatePlays { get; set; }
+
+    public int ShortTrackPlays { get; set; }
+
+    public int MaxPlaysInDay { get; set; }
+
+    public DateTime? MaxPlaysDay { get; set; }
+
+    public int DaysOverPlayLimit { get; set; }
+
+    public List<BottedCheckTopTrack> TopTracks { get; set; }
+
+    public BottedCheckTopTrack TopShortTrack { get; set; }
+
+    public string TopArtistName { get; set; }
+
+    public long TopArtistPlaycount { get; set; }
+}
+
+public class BottedCheckTopTrack
+{
+    public string Name { get; set; }
+
+    public string ArtistName { get; set; }
+
+    public long Playcount { get; set; }
+
+    public int? DurationMs { get; set; }
+}
+
 public class WhoKnowsSettings
 {
-    public WhoKnowsSettings()
-    {
-        this.RedirectsEnabled = true;
-    }
-
     public bool HidePrivateUsers { get; set; }
 
     public bool ShowBotters { get; set; }
@@ -42,5 +79,5 @@ public class WhoKnowsSettings
 
     public bool DisplayRoleFilter { get; set; }
 
-    public bool RedirectsEnabled { get; set; }
+    public bool RedirectsEnabled { get; set; } = true;
 }
